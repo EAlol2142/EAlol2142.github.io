dc721c93381dcc22f832d29693e74126b4f093cf
rostislav <ealol2142@gmail.com>
2023-06-05 16:53:16 +0300

    s

--- app.js
+++ app.js
@@ -8,30 +8,48 @@ tg.MainButton.color = "#2cab37";
 let item = "";
 let choice = "";
 
-let alum_pipes = document.getElementById("Aluminum_pipes");
-let boards = document.getElementById("Boards");
-let floor_til = document.getElementById("Floor_tiles");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
 
-alum_pipes.addEventListener("click", function(){
-    item = "Aluminum pipes";
+btn1.addEventListener("click", function(){
+    item = "1";
+    choice = btn1;
 });
 
-boards.addEventListener("click", function(){
-    item = "Boards";
+btn2.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали 2 кнопку");
+        item = "2";
+        tg.MainButton.show();
+    }
 });
 
-floor_til.addEventListener("click", function(){
-    item = "Floor tiles";
+btn3.addEventListener("click", function(){
+    if (tg.MainButton.isVisible){
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вы выбрали 3 кнопку");
+        item = "3";
+        tg.MainButton.show();
+    }
 });
 
 btn1.addEventListener("click", () => {
    
     let data = {
-        name: "Al",
+        name: "name",
     }
     tg.sendData(JSON.stringify(data));
 
     tg.close()
 });
 
+// Telegram.WebApp.onEvent("mainButtonClicked", function(){
+//     tg.sendData(item);
+// });
 
