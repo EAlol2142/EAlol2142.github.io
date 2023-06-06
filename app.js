035f6a0fce72e21ad8e3cbcf1946900285b9f066
rostislav <ealol2142@gmail.com>
2023-06-06 14:05:25 +0300

    a

--- app.js
+++ app.js
@@ -10,20 +10,6 @@ let btn_les = document.getElementById("les");
 let btn_plitka = document.getElementById("plitka");
 let btn_ugol = document.getElementById("ugol");
 
-btn_pipe.addEventListener("click", function () {
-    item = "pipe";
-});
-btn_les.addEventListener("click", function () {
-    item = "les";
-});
-btn_plitka.addEventListener("click", function () {
-    item = "plitka";
-});
-btn_ugol.addEventListener("click", function () {
-    item = "ugol";
-});
-
-
 
 btn_pipe.addEventListener("click", () => {
     let data = {
@@ -33,7 +19,44 @@ btn_pipe.addEventListener("click", () => {
     tg.close()
 });
 
+btn_les.addEventListener("click", () => {
+    let data = {
+        name: 'les',
+    }
+    tg.sendData(JSON.stringify(data));
+    tg.close()
+});
+
+btn_plitka.addEventListener("click", () => {
+    let data = {
+        name: 'plitka',
+    }
+    tg.sendData(JSON.stringify(data));
+    tg.close()
+});
+
+btn_ugol.addEventListener("click", () => {
+    let data = {
+        name: 'ugol',
+    }
+    tg.sendData(JSON.stringify(data));
+    tg.close()
+});
+
 // Telegram.WebApp.onEvent("mainButtonClicked", function(){
 //     tg.sendData(item);
+// });
+
+// btn_pipe.addEventListener("click", function () {
+//     item = "pipe";
+// });
+// btn_les.addEventListener("click", function () {
+//     item = "les";
+// });
+// btn_plitka.addEventListener("click", function () {
+//     item = "plitka";
+// });
+// btn_ugol.addEventListener("click", function () {
+//     item = "ugol";
 // });
 
